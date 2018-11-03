5b28560668c0be91b72c0ec45d8739fe5c91ab67
koshatnitca@pc <koshatnitca_21@mail.ru>
2018-11-04 02:36:53 +0700

    Word creature done

--- project_4.py
+++ project_4.py
@@ -1,3 +1,21 @@
 # Word creature
 
+import random
+from os import remove
+
+letters = ['А', 'Е', 'Н', 'О', 'С', 'Т']
+
+a = random.choice(letters)
+letters.remove(a)
+b = random.choice(letters)
+letters.remove(b)
+c = random.choice(letters)
+letters.remove(c)
+d = random.choice(letters)
+letters.remove(d)
+
+list_word = [a, b, c, d]
+str_word = (str(a+b+c+d))
+
+
 # Word solving
